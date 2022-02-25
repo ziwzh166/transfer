851adc7d3d0eb7ed5e1710ff260853f473d7d09c
zhao <ziwen.zhao@kemi.uu.se>
2022-02-25 15:27:30 +0100

    add rbycython

--- ex4.1 cython/rbf.pyx
+++ ex4.1 cython/rbf.pyx
@@ -2,18 +2,28 @@
 """
 Created on Fri Feb 25 12:09:55 2022
 
-@author: zzw
+@author: zhao
 """
 
-def rbf_cython(float X[] , float beta[] ,int theta):
+
+def rbf_network_cython(double X[][] , double beta[] ,int theta):
     cdef int N, D
     N = sizeof(X)/sizeof(X[0])
     D = sizeof(X)/sizeof(X[0][0])
-    return N,D
-int main(void)
-{float x [10][15]
- int N = rbf_cython(x)
- return}
+    cdef double Y[N]
+    cdef int i,j,d
+    double r
+    for i in range(N):
+        for j in range(N):
+            r = 0
+            for d in range(D):
+                r += (X[j, d] - X[i, d]) ** 2
+            r = r**0.5
+            Y[i] += beta[j] * exp(-(r * theta)**2)
+    return Y
+    
+
+
 
     
     
